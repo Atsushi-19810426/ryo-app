c6d37a21308bddaa3b8c01a8d3a63ae2c650d289
Atsushi-19810426 <atsushi19810426@gmail.com>
2026-06-29 08:29:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>self.clients.claim());
+const CACHE='gspot-ver59-sort-order';
+const FILES=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
